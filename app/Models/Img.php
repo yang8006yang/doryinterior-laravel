4c662ca762e48f0fd3a557e86c2a872aa5c7907c
doryyang <doryyang@heran.com.tw>
2023-07-04 10:48:08 +0800

    modified:   app/Models/Img.php 	modified:   app/Models/Project.php     關聯專案與圖片(一對多)

--- app/Models/Img.php
+++ app/Models/Img.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Img extends Model
 {
     use HasFactory;
+
+    function project() {
+        return $this->belongsTo(Project::class);
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -21,4 +22,8 @@ class Project extends Model
         'show',
         'master',
     ];
+
+    function imgs() {
+        return $this->hasMany(Img::class);
+    }
 }
